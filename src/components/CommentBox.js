1bbf6c1d24d11464851d32033ecccf9d9e21eb49
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-03-26 17:44:10 +0100

    new tests added

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux"
+import * as actions from "actions"
 
 class CommentBox extends React.Component {
   state = { comment: "" };
@@ -8,8 +10,7 @@ class CommentBox extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
 
-    //TODO - Call an action creator and
-    //       save the comment
+    this.props.saveComment(this.state.comment)
 
     this.setState({ comment: "" });
   };
@@ -27,4 +28,4 @@ class CommentBox extends React.Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
--- src/components/__tests__/commentBox.test.js
+++ src/components/__tests__/commentBox.test.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import CommentBox from 'components/commentBox';
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox from "components/commentBox";
+import Root from "Root";
 
-let wrapper 
+let wrapper;
 
-beforeEach( () => {
-    wrapper = mount(<CommentBox />); 
+beforeEach(() => {
+  wrapper = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  );
 });
 
-afterEach( () => {
-    wrapper.unmount();
-} );
-
-it('has a texta area and a button', () => {
-    expect(wrapper.find('textarea').length).toEqual(1);
-    expect(wrapper.find('button').length).toEqual(1);
+afterEach(() => {
+  wrapper.unmount();
 });
 
-it('has a text area that users can type in it', () => {
-    wrapper.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
-    })
+it("has a texta area and a button", () => {
+  expect(wrapper.find("textarea").length).toEqual(1);
+  expect(wrapper.find("button").length).toEqual(1);
+});
+
+describe("The text area", () => {
+  beforeEach(() => {
+    wrapper.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
     wrapper.update();
-    expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it("has a text area that users can type in it", () => {
+    expect(wrapper.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("submits form and checks if textArea has no text", () => {
+    wrapper.find("textarea").simulate("submit");
+    expect(wrapper.find("textarea").prop("value")).toEqual("");
+  });
 });
